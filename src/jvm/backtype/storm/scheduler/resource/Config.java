78058e1c0c48f3b08924707c2fa0525db4aebb8f
Jerry <jerry@ubuntu.(none)>
2014-11-30 15:14:49 -0800

    modifying config

--- src/jvm/backtype/storm/scheduler/resource/Config.java
+++ src/jvm/backtype/storm/scheduler/resource/Config.java
@@ -4,6 +4,6 @@ public class Config {
 	public final static String STORM_ROOT="../../../../../../";
 	public final static String LOG_PATH = "/tmp/";
 	public static String SCHEDULER_TYPE="";
-	public final static String NETWORK_DATA_FILE = STORM_ROOT+"data/NetworkData";
+	public final static String NETWORK_DATA_FILE = "/users/peng/storm-resource-aware-scheduler/data/NetworkData";
 	
 }
